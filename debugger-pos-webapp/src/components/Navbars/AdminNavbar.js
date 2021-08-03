81f9d8505c1ab6be67a036ab6b47627b35b98030
Chathura Dushmantha <dushmantha.1989@gmail.com>
2021-08-03 14:58:05 +0530

    Added bells and whistles

--- debugger-pos-webapp/src/components/Navbars/AdminNavbar.js
+++ debugger-pos-webapp/src/components/Navbars/AdminNavbar.js
@@ -15,61 +15,68 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React, { Component } from "react";
-import { useLocation } from "react-router-dom";
-import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
+import React, { Component } from 'react'
+import { useLocation } from 'react-router-dom'
+import { Navbar, Container, Nav, Dropdown, Button } from 'react-bootstrap'
+import Clock from 'react-live-clock'
 
-import routes from "../../routes.js";
+import routes from '../../routes.js'
 
 function Header() {
-  const location = useLocation();
-  const mobileSidebarToggle = (e) => {
-    e.preventDefault();
-    document.documentElement.classList.toggle("nav-open");
-    var node = document.createElement("div");
-    node.id = "bodyClick";
-    node.onclick = function () {
-      this.parentElement.removeChild(this);
-      document.documentElement.classList.toggle("nav-open");
-    };
-    document.body.appendChild(node);
-  };
-
-  const getBrandText = () => {
-    for (let i = 0; i < routes.length; i++) {
-      if (location.pathname.indexOf(routes[i].layout + routes[i].path) !== -1) {
-        return routes[i].name;
-      }
+    const location = useLocation()
+    const mobileSidebarToggle = (e) => {
+        e.preventDefault()
+        document.documentElement.classList.toggle('nav-open')
+        var node = document.createElement('div')
+        node.id = 'bodyClick'
+        node.onclick = function () {
+            this.parentElement.removeChild(this)
+            document.documentElement.classList.toggle('nav-open')
+        }
+        document.body.appendChild(node)
     }
-    return "Brand";
-  };
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
-          <Button
-            variant="dark"
-            className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
-            onClick={mobileSidebarToggle}
-          >
-            <i className="fas fa-ellipsis-v"></i>
-          </Button>
-          <Navbar.Brand
-            href="#home"
-            onClick={(e) => e.preventDefault()}
-            className="mr-2"
-          >
-            {getBrandText()}
-          </Navbar.Brand>
-        </div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2">
-          <span className="navbar-toggler-bar burger-lines"></span>
-          <span className="navbar-toggler-bar burger-lines"></span>
-          <span className="navbar-toggler-bar burger-lines"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav mr-auto" navbar>
-            {/* <Nav.Item>
+
+    const getBrandText = () => {
+        for (let i = 0; i < routes.length; i++) {
+            if (
+                location.pathname.indexOf(routes[i].layout + routes[i].path) !==
+                -1
+            ) {
+                return routes[i].name
+            }
+        }
+        return 'Brand'
+    }
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
+                    <Button
+                        variant="dark"
+                        className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
+                        onClick={mobileSidebarToggle}
+                    >
+                        <i className="fas fa-ellipsis-v"></i>
+                    </Button>
+                    <Navbar.Brand
+                        href="#home"
+                        onClick={(e) => e.preventDefault()}
+                        className="mr-2"
+                    >
+                        {getBrandText()}
+                    </Navbar.Brand>
+                </div>
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    className="mr-2"
+                >
+                    <span className="navbar-toggler-bar burger-lines"></span>
+                    <span className="navbar-toggler-bar burger-lines"></span>
+                    <span className="navbar-toggler-bar burger-lines"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="nav mr-auto" navbar>
+                        {/* <Nav.Item>
               <Nav.Link
                 data-toggle="dropdown"
                 href="#pablo"
@@ -80,7 +87,7 @@ function Header() {
                 <span className="d-lg-none ml-1">Dashboard</span>
               </Nav.Link>
             </Nav.Item> */}
-            {/* <Dropdown as={Nav.Item}>
+                        {/* <Dropdown as={Nav.Item}>
               <Dropdown.Toggle
                 as={Nav.Link}
                 data-toggle="dropdown"
@@ -125,7 +132,7 @@ function Header() {
                 </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown> */}
-            {/* <Nav.Item>
+                        {/* <Nav.Item>
               <Nav.Link
                 className="m-0"
                 href="#pablo"
@@ -135,9 +142,9 @@ function Header() {
                 <span className="d-lg-block"> Search</span>
               </Nav.Link>
             </Nav.Item> */}
-          </Nav>
-          <Nav className="ml-auto" navbar>
-            {/* <Nav.Item>
+                    </Nav>
+                    <Nav className="ml-auto" navbar>
+                        {/* <Nav.Item>
               <Nav.Link
                 className="m-0"
                 href="#pablo"
@@ -192,20 +199,51 @@ function Header() {
                 </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown> */}
-            <Nav.Item>
-              <Nav.Link
-                className="m-0"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="no-icon">Log out</span>
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+
+                        <Nav.Item>
+                            <Nav.Link
+                                className="m-0"
+                                href="#pablo"
+                                onClick={(e) => e.preventDefault()}
+                            >
+                                <span>
+                                    <h3 className="no-icon clock-container">
+                                        {new Date().toISOString().slice(0, 10)}
+                                    </h3>
+                                </span>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                className="m-0"
+                                href="#pablo"
+                                onClick={(e) => e.preventDefault()}
+                            >
+                                <span>
+                                    <h3 className="no-icon clock-container">
+                                        <Clock
+                                            format="HH:mm:ss"
+                                            interval={1000}
+                                            ticking={true}
+                                        />
+                                    </h3>
+                                </span>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                className="m-0"
+                                href="#pablo"
+                                onClick={(e) => e.preventDefault()}
+                            >
+                                <span className="no-icon">Log out</span>
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 }
 
-export default Header;
+export default Header
